95ab1f5b5fc780ee8eaca4c9e395859dd5a71a8f
MRXjs <95880266+MRXjs@users.noreply.github.com>
2022-01-02 20:03:31 +0530

    add new features notification,timer,timersStartBtn,timerStopBtn,timersResetBtn

--- options.js
+++ options.js
@@ -1,14 +1,18 @@
 const nameInput = document.getElementById("name-input");
+const timeInput = document.getElementById("time-input");
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", () => {
   const name = nameInput.value;
-  chrome.storage.sync.set(
-    {
-      name,
-    },
-    () => {
-      console.log(`Name is set to ${name}`);
-    }
-  );
+  const notificationTime = timeInput.value;
+  // store to chrome storage
+  chrome.storage.sync.set({
+    name,
+    notificationTime,
+  });
+});
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
+  nameInput.value = res.name ?? "???";
+  timeInput.value = res.notificationTime ?? 1000;
 });
--- popup.js
+++ popup.js
@@ -1,12 +1,51 @@
 const timeElement = document.getElementById("time");
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `The time is: ${currentTime}`;
+const nameElement = document.getElementById("name");
+const timerElement = document.getElementById("timer");
 
-chrome.action.setBadgeText(
-  {
-    text: "Time",
-  },
-  () => {
-    console.log("Fineshed setting badge text.");
-  }
-);
+// time update
+function updateTimeElement() {
+  // timer
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The timer is at: ${time} seconds`;
+  });
+
+  // timeElement
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
+}
+
+updateTimeElement();
+setInterval(updateTimeElement, 1000);
+
+// name
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "???";
+  nameElement.textContent = `Your name is: ${name}`;
+});
+
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+// startBtn
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  });
+});
+
+// stopBtn
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+});
+
+// resetBtn
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  });
+});
